9c421f07c0014e0c4e6b7dab1f9ba97e32637b22
chaewoon-Front <treechae38@naver.com>
2022-12-06 00:08:49 +0900

    ReactBlog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let posts = "강남 우동 맛집";
+  let [글제목, 글제목변경] = useState([
+    "강남 맛집 추천",
+    "성수 카페 추천",
+    "자바스크립트 책 추천",
+  ]);
+  let [좋아요, 좋아요변경] = useState(0);
+
+  function 함수() {
+    console.log(1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = "여자코트 추천";
+          글제목변경(copy);
+        }}
+      >
+        글수정
+      </button>
+
+      <div className="list">
+        <h4>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              좋아요변경(좋아요 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {좋아요}
+        </h4>
+        <p>12월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>12월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
+        <p>12월 17일 발행</p>
+      </div>
     </div>
   );
 }
