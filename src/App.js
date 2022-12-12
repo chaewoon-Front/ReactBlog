fc0ff214627ad4e75ca5e60dfe72e28db910971e
chaewoon-Front <treechae38@naver.com>
2022-12-12 22:09:10 +0900

    input 추가

--- src/App.js
+++ src/App.js
@@ -21,45 +21,6 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      {/* <button
-        onClick={() => {
-          let copy = [...글제목];
-          copy[0] = "여자코트 추천";
-          글제목변경(copy);
-        }}
-      >
-        글수정
-      </button> */}
-
-      {/* <div className="list">
-        <h4>
-          {글제목[0]}
-          <span
-            onClick={() => {
-              좋아요변경(좋아요 + 1);
-            }}
-          >
-            👍
-          </span>
-          {좋아요}
-        </h4>
-        <p>12월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>12월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            setModal(!modal);
-          }}
-        >
-          {글제목[2]}
-        </h4>
-        <p>12월 17일 발행</p>
-      </div> */}
-
       {[1, 2, 3].map(function (a, i) {
         return (
           <div className="list" key={i}>
@@ -107,6 +68,12 @@ function Modal(props) {
       >
         글 수정
       </button>
+      <input type="text" />
+      <input type="range" />
+      <input type="date" />
+      <input type="number" />
+      <textarea></textarea>
+      <select></select>
     </div>
   );
 }
